18354ffe92bd15da9ae717fdc5ec54cf7eeabeef
Jakub <kubkozak@gmail.com>
2023-05-10 09:59:38 +0200

    footer
    footer (zatial bez responzivity)

--- app/src/views/_components/Navbar.vue
+++ app/src/views/_components/Navbar.vue
@@ -6,7 +6,7 @@
           <img src="@/views/_components/_assets/LogoPerrito.svg" alt="" class="w-24 h-24">      
           <h1 class="text-5xl inter font-extralight mb-[0.460rem]">
             Perrito
-          </h1> 
+          </h1>
         </a>               
       </div>
       <div class="flex">
