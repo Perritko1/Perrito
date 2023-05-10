98e685a2351c9c6453b80204cd8040d6375eb1a4
luqeey <eliaslukas557@gmail.com>
2023-05-10 09:31:42 +0200

    +env

--- app/src/main.js
+++ app/src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/tailwind.css'
-import store from './store' // <-- Import the store object
+import store from './store'
+import axios from "axios"; // <-- Import the store object
 
 const app = createApp(App)
     .use(router)
     .use(store)
+    axios.defaults.baseURL = process.env.VUE_APP_AXIOS_URL;
+    app.config.globalProperties.$axios = axios;
 
 
 router.isReady().then(() => {
