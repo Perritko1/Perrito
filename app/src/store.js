dca9833d147d566ca63b8e695a970942281abe22
letovanec-wezeo <letovanec@wezeo.com>
2023-05-23 12:19:35 +0200

    Update store.js

--- app/src/store.js
+++ app/src/store.js
@@ -12,9 +12,49 @@ const store = createStore({
   mutations: {
     setData(state, payload) {
       state.data = payload;
-    }
+    },
+
+    auth_success(state, data) {
+			if (data.token) {
+				state.token = data.token
+				localStorage.setItem('perrito_vue_token', data.token)
+			}
+
+			if (data.user) {
+				state.user = data.user
+				console.log(state.user)
+				localStorage.setItem('perrito_vue_user', JSON.stringify(data.user))
+			}
+		},
+
+    auth_error(state) {
+			state.token = ''
+			localStorage.removeItem('perrito_vue_user')
+			localStorage.removeItem('perrito_vue_token')
+		},
   },
   actions: {
+
+    async register({ commit }, credentials){
+      try {
+        const response = await axios.post('', credentials)
+        commit('auth_success', response.data)
+      } catch(err) {
+        commit('auth_error')
+        console.error(err)
+      }
+    },
+
+    async login({ commit }, credentials){
+      try {
+        const response = await axios.post('/auth/login', credentials)
+        console.log(response)
+        commit('auth_success', response.data)
+      } catch(err) {
+        commit('auth_error')
+        console.error(err)
+      }
+    },
     // signup({commit}, credentials){
     //   return new Promise((resolve, reject) => {
 
@@ -30,6 +70,10 @@ const store = createStore({
     }
   },
   getters: {
+    isLoggedIn: (state) =>  {
+			return !!state.token
+		},
+
     // registerUser(user) {
     //   axios.post('/auth/register', user).then(response => {
     //     console.log(response);
